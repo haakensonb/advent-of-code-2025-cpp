7171eef15528b8bf599bee5bb2a300c444df9ff8
haakensonb <untamedhawk44@gmail.com>
2025-12-14 17:03:38 -0700

    Update main, for missing Day file

--- main.cpp
+++ main.cpp
@@ -3,16 +3,18 @@
 #include "day/day_1/day_1.h"
 #include "common/common.h"
 
-std::string sample_input = "L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82";
-
 int main() {
     auto files = Common::get_files_in_dir(PROJECT_ROOT "/puzzle_input");
 
     for (const auto& file : files) {
-        auto& day = Common::input_to_day_map[file];
-        std::cout << "File: " << file << std::endl;
-        std::cout << "  Part 1: " << day->solve_part_1() << std::endl;
-        std::cout << "  Part 2: " << day->solve_part_2() << std::endl;
+        if (Common::input_to_day_map.contains(file)){
+            auto& day = Common::input_to_day_map[file];
+            std::cout << "File: " << file << std::endl;
+            std::cout << "\tPart 1: " << day->solve_part_1() << std::endl;
+            std::cout << "\tPart 2: " << day->solve_part_2() << std::endl;
+        } else {
+            std::cout << "No implementation for file: " << file << std::endl;
+        }
     }
 
     return 0;
